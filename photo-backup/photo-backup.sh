c63e32435efb9ae0a8bae6fa6dfd312bb3050d26
Phil <philip.clamp1@gmail.com>
2018-12-07 19:00:38 +0000

    Fix script

--- photo-backup/photo-backup.sh
+++ photo-backup/photo-backup.sh
@@ -4,9 +4,9 @@ set -ex
 
 command -v aws || { echo "I require aws but it's not installed. Aborting." >&2; exit 1; }
 
-REMOTE_MAPPINGS_DIR=s3://photos
-LOCAL_MAPPINGS_DIR=/volume1/photos
+REMOTE_MAPPINGS_DIR=s3://clamped90-photos
+LOCAL_MAPPINGS_DIR=/volume1/photo
 
-aws s3 sync $LOCAL_MAPPINGS_DIR $REMOTE_MAPPINGS_DIR --dry-run
+aws s3 sync $LOCAL_MAPPINGS_DIR $REMOTE_MAPPINGS_DIR --dryrun
 
 exit 0
